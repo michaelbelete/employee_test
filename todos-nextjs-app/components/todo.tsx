3d7818dd3ef8dcc55c4e631bf8b9872c3fe110b3
michael belete <it.michael.belete@gmail.com>
2022-03-23 23:25:01 +0300

    finished updated

--- todos-nextjs-app/components/todo.tsx
+++ todos-nextjs-app/components/todo.tsx
@@ -3,7 +3,9 @@ import Checkbox from './checkbox'
 
 export default function Todo({ todo, reloadList }) {
   //TODO: implement toggle checkbox
-  const toggleCompleted = () => {}
+  const toggleCompleted = () => {
+    axios({url: 'api/todos/'+ todo._id, method: 'PUT', data: {text: todo.text, completed: true}}).then(reloadList)
+  }
 
   const deleteTodo = () => {
     axios.delete('/api/todos/' + todo._id).then(reloadList)
